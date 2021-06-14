47a578524f79d11f7b1c6b920f9aaaf402fcb3ec
yudialfayat <ayudi3521@gmail.com>
2021-06-14 09:23:19 +0700

    rapi rapi

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 function navLinkColl() {
-    // var bungkus = document.getElementById("navbarNavAltMarkup");
-    // var button = document.getElementById("buttonNavbar");
-    // if (bungkus.classList.contains("show"))
-    // {
-    //         button.classList.add("collapsed");
-    //         button.setAttribute("aria-expanded", false);
-    //         bungkus.classList.remove("show");
-    //         alert("dsad");
-    // }
-
     var bungkuss = document.getElementsByClassName("navbar-collapse");
     var buttons = document.getElementsByClassName("navbar-toggler");
     if (bungkuss[0].classList.contains("show")) {
@@ -23,26 +13,4 @@ function navLinkColl() {
             element.classList.remove("show");
         }
     }
-}
-
-function getRandomColor() {
-    var colors = [
-        ["#ff9a9e", "#f6416c"],
-        ["#9ef7f1", "#03eeff"],
-        ["#fce5c7", "#f7a33b"],
-        ["#ffc4fc", "#ff4af6"],
-        // ["#f8fab9", "#f9ff45"], // Yellow
-    ];
-    var idx = Math.floor(Math.random() * colors.length);
-    var color = colors[idx];
-
-    return color;
-}
-
-function setBackgroundColor() {
-    var color = getRandomColor();
-    document.body.style.backgroundColor = color[1];
-    document.getElementById("main").style.backgroundImage = "linear-gradient(to right bottom, " + color[0] + " 10%" + ", " + color[1] + " 100%" + ")";
-    // document.getElementById("circle1").style.background = color[1];
-    // document.getElementById("circle2").style.background = color[1];
 }
