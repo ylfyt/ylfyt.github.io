f5a490c7875c5713d5b4e9f37ac152f3f25be05e
yudialfayat <ayudi3521@gmail.com>
2022-03-13 23:23:32 +0700

    Glass and Navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,39 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+<script>
+	import Navbar from './components/Navbar.svelte';
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+<Navbar />
+<main class="glass">
+	<p>Hello</p>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: 'Poppins', sans-serif;
+		transition: all 0.3s ease;
+	}
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+	:root {
+		background-image: linear-gradient(to right bottom, #ffd89b, #19547b);
+		background: -webkit-linear-gradient(to left top, #f64f59, #c471ed, #12c2e9);
+		background: linear-gradient(to left top, #f64f59, #c471ed, #12c2e9);
+		background-repeat: no-repeat;
+		height: 100%;
+	}
+	:global(.glass) {
+		background: linear-gradient(
+			to right bottom,
+			rgba(255, 255, 255, 0.3),
+			rgba(255, 255, 255, 0.2)
+		);
+		box-shadow: 0 20px 50px rgba(0, 0, 0, 0.15);
+		border-top: 1px solid rgba(255, 255, 255, 0.5);
+		border-left: 1px solid rgba(255, 255, 255, 0.5);
+		border-radius: 15px;
+		padding: 10px;
+	}
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<div class="glass navbar">
+	<div class="nav-brand">Yudi Alfayat</div>
+	<div class="nav-items">
+		<div>About</div>
+		<div>Projects</div>
+		<div>Contact</div>
+	</div>
+</div>
+
+<style>
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		padding-right: 30px;
+		padding-left: 30px;
+	}
+
+	.nav-brand {
+		font-size: large;
+		font-weight: 500;
+	}
+
+	.nav-items {
+		display: flex;
+		gap: 30px;
+	}
+</style>
