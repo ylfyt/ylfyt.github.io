1e8a0500c203106df1d61c3ae0c29086cf3c3f76
yudialfayat <ayudi3521@gmail.com>
2022-03-14 00:44:07 +0700

    Project type

---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { IProject } from '../interfaces/IProject';
+	export let project: IProject;
+</script>
+
+<div>
+	{project.title}
+</div>
--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -1,1 +1,10 @@
-<div>Projects</div>
+<script lang="ts">
+	import ProjectCard from '../components/ProjectCard.svelte';
+	import { projects } from '../utils/db';
+</script>
+
+<div class="projects-container">
+	{#each projects as project}
+		<ProjectCard {project} />
+	{/each}
+</div>
