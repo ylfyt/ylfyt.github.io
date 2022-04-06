952be82bf2584878e85753e09304e8ec8977bafd
yudialfayat <ayudi3521@gmail.com>
2022-04-07 05:46:06 +0700

    Update projects

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -10,9 +10,9 @@ const dbProjects: IProject[] = [
 			dir: '/ylfyt'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/ylfyt.github.io' }],
-		tags: ['Svelte', 'Typescript', 'Vite'],
+		tags: ['Svelte', 'Typescript', 'Vite', 'Glassmorphism'],
 		description:
-			'Website portfolio yang dibuat dengan menggunakan framework Svelte dan bahasa Typescript. Pada website ini menggunakan Vite sebagai build tools-nya'
+			'Website portfolio yang dibuat dengan menggunakan framework Svelte dan bahasa Typescript. Design dari website ini adalah Glass Morphism website. Build tools yang digunakan adalah Vite.'
 	},
 	{
 		title: 'Led strips system',
@@ -35,7 +35,7 @@ const dbProjects: IProject[] = [
 			dir: '/acrylic-lamp'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/Acrylic-Lamp-System' }],
-		tags: ['Arduino', 'FastLED'],
+		tags: ['Arduino', 'Fusion360', 'FastLED', 'Bluetooth'],
 		description:
 			'Acrylic lamp dengan menggunakan arduino uno sebagai microcontroller. Led yang yang digunakan adalah Addressable Led WS2812b. Untuk mengontrol led tersebut digunakan library FastLED. Acrylic lamp ini menggunakan 2 jenis controller untuk pengguna, pertama yaitu touch sensor dan yang kedua adalah secara wireless dengan menggunakan aplikasi android yang terhubung melalui bluetooth. Aplikasi android tersebut dibuat dengan menggunakan platform kodular.io. Sedangkan base yang menjadi tempat acrylicnya dibuat dengan menggunakan Fusion360, lalu dicetak dengan menggunakan 3d printer.'
 	},
@@ -46,7 +46,7 @@ const dbProjects: IProject[] = [
 			dir: '/mood-lamp'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/moodlamp-with-async-effect' }],
-		tags: ['Arduino', 'FastLED'],
+		tags: ['Arduino', 'FastLED', 'Bluetooth'],
 		description:
 			'Mood lamp ini dibuat dengan menggunakan arduino uno sebagai microcontrollernya. Led yang digunakan adalah Addressable Led ws2812b dengan library untuk animasinya menggunakan FastLED. Animasi pada moodlamp ini merupakan modifikasi dari animasi berikut. Modifikasi yang dilakukan adalah mengganti penggunaan delay dengan millis. Hal ini bertujuan untuk mengatasi masalah ketika perlunya membaca nilai komunikasi serial, karena lampu ini dilengkapi dengan sensor bluetooth yang dapat terhubung ke android dengan menggunakan aplikasi yang dibuat dengan kodular.io.'
 	},
@@ -64,7 +64,6 @@ const dbProjects: IProject[] = [
 		description:
 			'Aplikasi untuk berbelanja barang-barang yang bisa di-custom sesuai keinginan pembeli. Aplikasi ini terdiri dari server dan frontend yang dibuat dengan Typescript. Pada bagian server menggunakan GraphQL sebagai komunikasi API-nya, dan untuk database menggunakan MongoDb dan MikroOrm sebagai Object relational mapping-nya. Selain itu framework yang digunakan adalah ExpressJs.'
 	},
-
 	{
 		title: 'Glassmorphism Portfolio Website',
 		images: {
@@ -72,30 +71,31 @@ const dbProjects: IProject[] = [
 			dir: '/glassmorphism'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/Glassmorphism-Portfolio-Website' }],
-		tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+		tags: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'Glassmorphism'],
 		description:
-			'Website portfoilio Glassmorphism. Website ini dibuat dengan menggunakan HTML, CSS, Javascript, dan Bootstrap'
+			'Website Portfolio Glass Morphism. Website ini dibuat dengan menggunakan vanilla HTML, CSS, Javascript, dan sedikit tambahan Bootstrap.'
 	},
 	{
-		title: 'Path finding using A-Star algorithm.',
+		title: 'Pathfinder using the A* algorithm.',
 		images: {
 			count: 1,
 			dir: '/pathfinding'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/A-Star_Hospital-Recommendation' }],
-		tags: ['Java', 'JavaFX'],
+		tags: ['A*', 'Java', 'JavaFX'],
 		description:
-			'Path finding dengan menggunakan Algoritmat A*. Aplikasi ini dibuat dengan menggunakan Java dengan Framework JavaFX'
+			'Pathfinder dengan menggunakan Algoritma A*. Aplikasi bertujuan untuk mencari jalur terdekat menuju rumah sakit. Bahasa yang digunakan adalah Java dengan Framework JavaFX.'
 	},
 	{
-		title: 'Perlu Dilindungi',
+		title: 'Perlu Dilindungi Android App',
 		images: {
 			count: 5,
 			dir: '/perlu-dilindungi'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/perlu-dilindungi-android-app' }],
-		tags: ['Kotlin', 'Android'],
-		description: 'PerluDilindungi App, create with kotlin android'
+		tags: ['Android', 'Kotlin'],
+		description:
+			'Perlu Dilindungi App. Aplikasi ini merupakan aplikasi android yang memiliki beberapa fitur seperti mencari daftar berita tentang covid, mencari faskes terdekat, bookmark, dan melakukan check in. Bahasa yang digunakan adalah kotlin. Aplikasi ini dibuat untuk memenuhi tugas mata kuliah Pengembangan Aplikasi pada Platform Khusus.'
 	},
 	{
 		title: 'Chrome Tabs Media Controller Extension',
@@ -104,8 +104,9 @@ const dbProjects: IProject[] = [
 			dir: '/tab-controller'
 		},
 		repositories: [{ url: 'https://github.com/ylfyt/chrome-tabs-media-controller-extension' }],
-		tags: ['Javascript', 'Chrome'],
-		description: 'Media controller for google chrome browser'
+		tags: ['Chrome', 'Extension', 'Javascript'],
+		description:
+			'Chrome Tabs Media Controller Extension. Media controller untuk browser chrome dan edge. Extension ini dibuat dengan menggunakan javascript dengan manifest versi 2. Fitur yang dimiliki adalah mute/unmute tab’s audio dan play/pause youtube tabs.'
 	}
 ];
 
