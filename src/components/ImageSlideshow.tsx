d2ce8bdab077a1d666926643f050eb56ab42894b
ylfyt <ayudi3521@gmail.com>
2022-07-01 23:03:49 +0700

    Feature: Fullscreen image

--- src/components/ImageSlideshow.tsx
+++ src/components/ImageSlideshow.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight, FaSearch } from 'react-icons/fa';
 import Button from './button';
 
 interface ImageSlideShowProps {
@@ -12,6 +12,7 @@ interface ImageSlideShowProps {
 const ImageSlideShow: FC<ImageSlideShowProps> = ({ images }) => {
 	const [idx, setIdx] = useState(1);
 	const [direction, setDirection] = useState('');
+	const [fullscreen, setFullscreen] = useState(false);
 
 	const slide = (isNext: boolean) => {
 		setDirection(isNext ? 'animate-slideFromLeft' : 'animate-slideFromRight');
@@ -40,13 +41,29 @@ const ImageSlideShow: FC<ImageSlideShowProps> = ({ images }) => {
 				})}
 			</div>
 			<div className="absolute flex justify-between w-full sm:px-6 px-2 top-3/4 sm:top-1/2 -translate-y-1/2">
-				<Button onClick={() => slide(false)} className="sm:text-4xl px-1 bg-gray-400 opacity-30 sm:opacity-10 hover:opacity-60 sm:px-2 sm:py-6 rounded-l-3xl rounded-r-md hover:text-color0">
+				<Button onClick={() => slide(false)} className="sm:text-4xl px-1 bg-gray-400 opacity-30 sm:opacity-20 hover:opacity-60 sm:px-2 sm:py-6 rounded-l-3xl rounded-r-md hover:text-color0">
 					<FaChevronLeft className="opacity-100" />
 				</Button>
-				<Button onClick={() => slide(true)} className="sm:text-4xl bg-gray-400 opacity-30 sm:opacity-10 hover:opacity-60 px-1 py-4 sm:px-2 sm:py-6 rounded-r-2xl rounded-l-md hover:text-color0">
+				<Button onClick={() => slide(true)} className="sm:text-4xl bg-gray-400 opacity-30 sm:opacity-20 hover:opacity-60 px-1 py-4 sm:px-2 sm:py-6 rounded-r-2xl rounded-l-md hover:text-color0">
 					<FaChevronRight className="opacity-100" />
 				</Button>
 			</div>
+			<div className="absolute text-color0 top-2 right-3 text-lg z-20">
+				<Button onClick={() => setFullscreen(true)} className="opacity-60 hover:opacity-100">
+					<FaSearch className="opacity-100" />
+				</Button>
+			</div>
+			{fullscreen && (
+				<div className="fixed top-0 h-screen z-20 w-full flex items-center">
+					<div className="flex justify-center items-center w-full max-h-[80vh] min-h-[55vh] opacity-100 relative z-50">
+						<button onClick={() => setFullscreen(false)} className="absolute lg:right-40 lg:top-6 top-2 right-4 text-2xl flex items-center py-0 opacity-60">
+							&#10006;
+						</button>
+						<img className="max-h-[75vh] opacity-100 sm:max-w-[90vw] lg::max-w-[60vw]" src={`${images.dir}/${idx}.png`} alt="" />
+					</div>
+					<div className="fixed bg-gray-500 h-screen w-full z-20 opacity-80"></div>
+				</div>
+			)}
 		</div>
 	);
 };
