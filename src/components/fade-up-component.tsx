2ba8b6ac1646f7059b21849d2a8e1101c10146a4
ylfyt <ayudi3521@gmail.com>
2022-07-27 18:00:21 +0700

    Fix Transition bug

--- src/components/fade-up-component.tsx
+++ src/components/fade-up-component.tsx
@@ -9,7 +9,15 @@ interface FadeUpComponentProps {
 const FadeUpComponent: FC<FadeUpComponentProps> = ({ children, order, mobileOrder }) => {
 	if (!mobileOrder) mobileOrder = order;
 
-	return <div className={`use-transition show w-full flex justify-center items-center show-${mobileOrder} sm:show-${order}`}>{children}</div>;
+	return (
+		<div
+			className={`use-transition show w-full flex justify-center items-center ${mobileOrder === 1 ? 'show-1' : mobileOrder === 2 ? 'show-2' : 'show-3'} ${
+				order === 1 ? 'sm:show-1' : order === 2 ? 'sm:show-2' : 'sm:show-3'
+			}`}
+		>
+			{children}
+		</div>
+	);
 };
 
 export default FadeUpComponent;
