ec917e9d747a70ade0a69107590c620b4adee4e3
ylfyt <ayudi3521@gmail.com>
2022-10-07 14:26:05 +0700

    Change memory to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import App from './App';
 import RootProvider from './contexts/root';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<MemoryRouter basename={import.meta.env.BASE_URL}>
+		<BrowserRouter basename={import.meta.env.BASE_URL}>
 			<RootProvider>
 				<App />
 			</RootProvider>
-		</MemoryRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );
