715f2722c5258215df3f2911e1ea3e5fb25dc56b
ylfyt <ayudi3521@gmail.com>
2022-10-07 14:30:55 +0700

    Back to MemoryRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import {  MemoryRouter } from 'react-router-dom';
 import App from './App';
 import RootProvider from './contexts/root';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter basename={import.meta.env.BASE_URL}>
+		<MemoryRouter basename={import.meta.env.BASE_URL}>
 			<RootProvider>
 				<App />
 			</RootProvider>
-		</BrowserRouter>
+		</MemoryRouter>
 	</React.StrictMode>
 );
