35c8984360b630804eb06f75eb1dffcda8aec1d0
ylfyt <yalfayat@gmail.com>
2024-02-15 17:48:59 +0700

    Adding error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Icon from '@iconify/svelte';
+</script>
+
+<div class="grid min-h-dvh place-items-center">
+    <div class="flex flex-col items-center">
+        <Icon class="text-9xl text-red-500" icon="tabler:face-id-error" />
+        <p class="text-2xl font-semibold text-color0">404</p>
+        <p class="text-color0">It looks like something is missing!</p>
+    </div>
+</div>
